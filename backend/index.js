188dffc6ec3ba5333b767354185dedd2de961224
Mayur <madankarmayur5@gmail.com>
2025-01-31 13:24:52 +0530

    url changed

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: "https://job-portal-opgv.onrender.com",
   credentials: true
 };
 
@@ -41,7 +41,6 @@ app.use(express.static(path.join(__dirname, "../frontend/dist")));
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "../frontend/dist/index.html"));
 });
-
 
 app.listen(PORT, () => {
   connectDB();
